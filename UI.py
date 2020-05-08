0f8f158c5c2e1584b104fe849e9c3deff38250f2
nakedlydog <mary.rybalkina@gmail.com>
2020-05-08 15:00:40 +0300

    Added function 'select  file'

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ import time
 import tkinter as tk
 import imageio
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfilename
 
 global pause_video
 
@@ -16,17 +17,14 @@ def video_frame_generator():
 
     start_time = current_time()
     _time = 0
+
     for frame, image in enumerate(video.iter_data()):
 
-        # turn video array into an image and reduce the size
         image = Image.fromarray(image)
         image.thumbnail((750, 750), Image.ANTIALIAS)
 
-        # make image in a tk Image and put in the label
         image = ImageTk.PhotoImage(image)
 
-        # introduce a wait loop so movie is real time -- asuming frame rate is 24 fps
-        # if there is no wait check if time needs to be reset in the event the video was paused
         _time += 1 / 24
         run_time = current_time() - start_time
         while run_time < _time:
@@ -49,33 +47,45 @@ def _start():
     pause_video = False
 
 
-def create_image_for_buttons(path, lenght, height):
-    play_img = Image.open(path)
-    play_img = play_img.resize((lenght, height), Image.ANTIALIAS)
-    return ImageTk.PhotoImage(play_img)
+def _choose_file():
+    text = askopenfilename(defaultextension='.mp4', filetypes=[('Video files','*.mp4'), ('All files','*.*')])
+    print(text)
+    return text
 
+
+def create_image_for_buttons(path, length, height):
+    icon = Image.open(path)
+    icon = icon.resize((length, height), Image.ANTIALIAS)
+    return ImageTk.PhotoImage(icon)
 
 
 if __name__ == "__main__":
 
     root = tk.Tk()
-    root.title('Video in tkinter')
+    root.title('Videoan analisys')
 
     my_label = tk.Label(root)
     my_label.pack()
 
-    play_img = create_image_for_buttons('icon_play.png', 40, 30)
-    pause_img = create_image_for_buttons('icon_pause.png', 40, 30)
+    #create icons
+    play_icon = create_image_for_buttons('icon_play.png', 40, 30)
+    pause_icon = create_image_for_buttons('icon_pause.png', 40, 30)
 
-    tk.Button(root, image=play_img, command=_start).pack(side=tk.LEFT)
-    tk.Button(root, image=pause_img, command=_stop).pack(side=tk.LEFT)
+
+    #create buttons
+    tk.Button(root, image=play_icon, command=_start).pack(side=tk.LEFT)
+    tk.Button(root, image=pause_icon, command=_stop).pack(side=tk.LEFT)
+    a = tk.Button(root, text='Choose the video file', command=_choose_file).pack(side=tk.LEFT)
 
     pause_video = False
+
     movie_frame = video_frame_generator()
 
     while True:
+
         if not pause_video:
             frame_number, frame = next(movie_frame)
+
             my_label.config(image=frame)
 
         root.update()
