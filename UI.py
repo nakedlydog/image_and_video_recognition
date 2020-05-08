b4f3c6fb52aa955a328671895c4638d69c37c42b
nakedlydog <mary.rybalkina@gmail.com>
2020-05-08 18:36:33 +0300

    Added UI, functions for video 'play', 'pause', 'choose file', 'take a snapshot'

--- UI.py
+++ UI.py
@@ -1,93 +1,95 @@
 import time
-import tkinter as tk
-import imageio
-from PIL import Image, ImageTk
+import tkinter
+import PIL.Image
+import PIL.ImageTk
+import cv2
 from tkinter.filedialog import askopenfilename
 
-global pause_video
+from _VideoCapture import _VideoCapture
 
 
-video_name = "my_video.mp4"
-video = imageio.get_reader(video_name)
+class App:
+    def __init__(self, window, window_title, video_source=0):
+
+        self.window = window
+        self.window.title(window_title)
+
+        self.path = 0
+        self.video = _VideoCapture(video_source)
+
+        self.canvas = tkinter.Canvas(window, width=700, height=400)
+        self.canvas.pack()
+
+        self.button_play = tkinter.Button(window, text='Play', width=20, command=self.play)
+        self.button_play.pack(side=tkinter.LEFT) # , expand=True)
+
+        self.button_pause = tkinter.Button(window, text='Pause', width=20, command=self.pause)
+        self.button_pause.pack(side=tkinter.LEFT) # , expand=True)
+
+        self.button_choose_file = tkinter.Button(window, text='Choose video-file', width=20, command=self.choose_file)
+        self.button_choose_file.pack(side=tkinter.LEFT)
+
+        self.button_snapshot = tkinter.Button(window, text='Snapshot!', width=20, command=self.take_snapshot)
+        self.button_snapshot.pack(side=tkinter.LEFT)  # , expand=True)
+
+        self.is_pause_video = True
+        self.is_new_video_choosen = False
+
+        self.delay = 15
+        self.update()
+
+        self.window.mainloop()
+
+    def update(self):
+        print(22)
+        if self.is_new_video_choosen:
+            self.video = _VideoCapture(self.path)
+            self.is_new_video_choosen = False
 
 
-def video_frame_generator():
-    def current_time():
-        return time.time()
 
-    start_time = current_time()
-    _time = 0
+        ret, frame = self.video.get_frame()
 
-    for frame, image in enumerate(video.iter_data()):
+        while True:
+            if self.is_pause_video == False:
+                break
+            self.window.update()
 
-        image = Image.fromarray(image)
-        image.thumbnail((750, 750), Image.ANTIALIAS)
-
-        image = ImageTk.PhotoImage(image)
-
-        _time += 1 / 24
-        run_time = current_time() - start_time
-        while run_time < _time:
-            run_time = current_time() - start_time
-        else:
-            if run_time - _time > 0.1:
-                start_time = current_time()
-                _time = 0
-
-        yield frame, image
+        if ret:
+            image = PIL.Image.fromarray(frame)
+            image.thumbnail((750, 750), PIL.Image.ANTIALIAS)
+            self.photo = PIL.ImageTk.PhotoImage(image=image)
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
 
 
-def _stop():
-    global pause_video
-    pause_video = True
+        self.window.after(self.delay, self.update)
+
+    def take_snapshot(self):
+        ret, frame = self.video.get_frame()
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def play(self):
+        self.is_pause_video = False
+
+    def pause(self):
+        self.is_pause_video = True
+
+    def choose_file(self):
+        self.path = askopenfilename(defaultextension='.mp4', filetypes=[('Video files', '*.mp4'), ('All files', '*.*')])
+        self.is_new_video_choosen = True
+        print(self.path)
+
+        # return text
 
 
-def _start():
-    global pause_video
-    pause_video = False
+
+App(tkinter.Tk(), 'VideoAnalysis')
 
 
-def _choose_file():
-    text = askopenfilename(defaultextension='.mp4', filetypes=[('Video files','*.mp4'), ('All files','*.*')])
-    print(text)
-    return text
 
 
-def create_image_for_buttons(path, length, height):
-    icon = Image.open(path)
-    icon = icon.resize((length, height), Image.ANTIALIAS)
-    return ImageTk.PhotoImage(icon)
+# , 'my_video.mp4')
 
 
-if __name__ == "__main__":
 
-    root = tk.Tk()
-    root.title('Videoan analisys')
-
-    my_label = tk.Label(root)
-    my_label.pack()
-
-    #create icons
-    play_icon = create_image_for_buttons('icon_play.png', 40, 30)
-    pause_icon = create_image_for_buttons('icon_pause.png', 40, 30)
-
-
-    #create buttons
-    tk.Button(root, image=play_icon, command=_start).pack(side=tk.LEFT)
-    tk.Button(root, image=pause_icon, command=_stop).pack(side=tk.LEFT)
-    a = tk.Button(root, text='Choose the video file', command=_choose_file).pack(side=tk.LEFT)
-
-    pause_video = False
-
-    movie_frame = video_frame_generator()
-
-    while True:
-
-        if not pause_video:
-            frame_number, frame = next(movie_frame)
-
-            my_label.config(image=frame)
-
-        root.update()
-
-    root.mainloop()
