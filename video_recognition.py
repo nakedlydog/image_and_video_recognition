9e25e0783d2bbb989689d48f83f2930ffff00c4a
nakedlydog <mary.rybalkina@gmail.com>
2020-05-09 07:21:32 +0300

    Added log file

--- video_recognition.py
+++ video_recognition.py
@@ -12,6 +12,8 @@ class VideoRecognition:
         self.detector.setModelPath(os.path.join(self.execution_path, "resnet50_coco_best_v2.0.1.h5"))
         self.detector.loadModel()
         self.start_process(output_video=output_video, input_video=input_video, type=type)
+        self.count = 0
+        self.text = []
         print(self.video_path)
 
     def start_process(self, output_video, input_video, type):
@@ -35,12 +37,19 @@ class VideoRecognition:
                 minimum_percentage_probability=50)
 
     def forSeconds(self, second_number, output_arrays, count_arrays, average_output_count):
+        text_file = open('res.txt', 'a')
         print("SECOND : ", second_number)
-        print("Array for the outputs of each frame ", output_arrays)
-        print("Array for output count for unique objects in each frame : ", count_arrays)
-        print("Output average count for unique objects in the last second: ", average_output_count)
-        if 'person' in average_output_count:
-            print('PERRSSSSSOOOOOOOONNN')
+        print("Array for the outputs of each frame ", output_arrays, file=text_file)
+        print("Array for output count for unique objects in each frame : ", count_arrays, file=text_file)
+        print("Output average count for unique objects in the last second: ", average_output_count, file=text_file)
+        counter = 0
+        if self.count / 5 == 0:
+            for el in output_arrays[1]:
+                if counter == 0:
+                    counter += 1
+                    pass#self.text.append(' ', el['name'], ' ')
+                    continue
+                pass#self.text.append('and ', el['name'], ' ')
         print("------------END OF A SECOND --------------")
 
 
